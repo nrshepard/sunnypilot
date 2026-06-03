1703d52552987bf191327ab96717cf5471b5bdab
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2026-06-02 17:25:24 -0700

    lead indicator

--- common/params_keys.h
+++ common/params_keys.h
@@ -166,6 +166,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"InteractivityTimeout", {PERSISTENT | BACKUP, INT, "0"}},
     {"IsDevelopmentBranch", {CLEAR_ON_MANAGER_START, BOOL}},
     {"IsReleaseSpBranch", {CLEAR_ON_MANAGER_START, BOOL}},
+    {"LeadCarIndicator", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"LastGPSPositionLLK", {PERSISTENT, STRING}},
     {"LeadDepartAlert", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"MaxTimeOffroad", {PERSISTENT | BACKUP, INT, "1800"}},
